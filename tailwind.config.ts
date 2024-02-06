2180e9abf12219a1238d3fe01dc180f9fd5721a0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-06 14:07:15 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      text: "#120F0C",
+      red: "#8D0F1B",
+      background: "#A78E88",
+      white: "#FFFFFF",
+      gray: "#E8E3E2",
+      beige: "#E6DFDE",
+      beigeDark: "#A78E88"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
