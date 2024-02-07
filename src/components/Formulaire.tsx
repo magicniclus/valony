bd88c24360997f066927e825dac55ca65e06dbfb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-07 13:48:21 +0100

    Create Formulaire.tsx

---
+++ src/components/Formulaire.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { use, useEffect } from 'react';
+import StepOne from './formStep/StepOne';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../redux/store';
+import { stopFocus, focus as focusAction } from '@/redux/formFocusSlice';
+
+
+const Formulaire = () => {
+
+    const focus = useSelector((state: RootState) => state.focus);
+
+    const dispatch = useDispatch();
+
+    return (
+        <form className='mt-5' 
+              onFocus={() => dispatch(focusAction())} 
+              onBlur={(e) => {
+                  // Empêche le déclenchement de onBlur lors du passage entre les éléments du formulaire
+                  if (!e.currentTarget.contains(e.relatedTarget)) {
+                      dispatch(stopFocus());
+                  }
+              }}>
+            <StepOne />
+        </form>
+    );
+};
+
+export default Formulaire;
