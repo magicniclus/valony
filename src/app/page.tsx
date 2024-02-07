af2b111c8e27ad33b801776eed6422ccaf4d854c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-07 13:48:17 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import Loader from "@/components/Loader";
+import { stopLoading } from "@/redux/loadingSlice";
+import Hero from "@/components/Hero";
 
 export default function Home() {
 
   const dispatch = useDispatch();
   const isLoading = useSelector((state: RootState) => state.loading.isLoading);
 
+  useEffect(() => {
+    if(isLoading){
+      setTimeout(() => {
+        dispatch(stopLoading()) 
+      }, 2000)
+    }
+  },[])
 
   if (isLoading) {
     return <Loader />;
   }
 
-  // Sinon, retournez le reste de votre composant
   return (
     <>
-      <header>
-
-      </header>
-      <main><h1>Hello world</h1></main>
+      <main>
+        <Hero />
+      </main>
       <footer></footer>
     </>
   );
