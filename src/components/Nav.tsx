34d2971d43e3fb6a7e1f4db1ab340f68fb1423e4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-07 15:48:58 +0100

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -28,8 +28,8 @@ const Nav : React.FC<NavProps> = ({language}) => {
             `}</style>
             <div className='w-full bg-transparent flex justify-end py-1 px-4'>
                 <div className='flex text-[18px]'>
-                    <button className={`buttonOne w-full h-[50px] font-bold mr-4 relative ${languageUpdate === "fr" ? "text-textClear" : "text-white"}`}>FR</button>
-                    <button className={`buttonTwo w-full h-[50px] text-textClear font-bold relative ${languageUpdate === "en" ? "text-textClear" : "text-white"}`}>EN</button>
+                    <button className={`${languageUpdate !== "fr" ? "buttonOne" : ""} w-full h-[50px] font-bold mr-4 relative ${languageUpdate === "fr" ? "text-textClear" : "text-white"}`}>FR</button>
+                    <button className={`${languageUpdate !== "en" ? "buttonTwo" : ""} w-full h-[50px] text-textClear font-bold relative ${languageUpdate === "en" ? "text-textClear" : "text-white"}`}>EN</button>
                 </div>
             </div>
         </>
