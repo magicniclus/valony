6289a4a15d1d0bbd8eccf4ef9dda98da73cd6d76
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-08 14:18:25 +0100

    Update BluePresentation.tsx

--- src/components/BluePresentation.tsx
+++ src/components/BluePresentation.tsx
@@ -9,11 +9,12 @@ type BluePresentationProps = {
         navPourcentage: number;
         title: string;
         susTitle: string;
-        list: [string, string, string, string, string?];
+        list: [string, string, string, string, string?, string?];
         button: string;
+        src: string;
     };
 
-const BluePresentation : React.FC<BluePresentationProps> = ({ navTitle, navPourcentage, title, susTitle, list, button }) => {
+const BluePresentation : React.FC<BluePresentationProps> = ({ navTitle, navPourcentage, title, susTitle, list, button, src }) => {
     return (
         <section className='flex'>
             <div className='w-[121px] min-h-[625px] bg-blueClear px-4 py-6 hidden lg:flex flex-col items-center justify-between'>
@@ -27,8 +28,8 @@ const BluePresentation : React.FC<BluePresentationProps> = ({ navTitle, navPourc
                 <ForwardLine pourcentage={navPourcentage} color="white" />
             </div>
             <div className='w-full flex md:flex-row flex-col'>
-                <div className='md:w-[50%] w-full min-h-[559px] overflow-hidden flex justify-between items-center bg-gray bg-cover bg-center' style={{"backgroundImage": "url(/images/oleron.jpg)"}}></div>
-                <div className='md:w-[50%] w-full min-h-[559px] overflow-hidden flex justify-between items-center bg-blueClearClear flex-col px-14 py-8'>
+                <div className='md:w-[50%] w-full min-h-[559px] overflow-hidden md:flex hidden justify-between items-center bg-gray bg-cover bg-center' style={{"backgroundImage": `url(/images/${src})`}}></div>
+                <div className='md:w-[50%] w-full min-h-[559px] overflow-hidden flex justify-between items-start bg-blueClearClear flex-col px-14 py-8'>
                     <h2 className='text-[45px] font-bold text-blueClear leading-[50px]'>
                         {title}
                     </h2>
@@ -38,14 +39,14 @@ const BluePresentation : React.FC<BluePresentationProps> = ({ navTitle, navPourc
                     <ul className='text-[14px] font-outfit text-text mt-3'>
                         {
                             list.map((item, index) => (
-                                <li key={index} className='flex items-center'>
+                                <li key={index} className='flex items-center mt-2'>
                                     <span className='mr-2'>-</span>
                                     {item}
                                 </li>
                             ))
                         }
                     </ul>
-                    <div className='w-full mt-3'>
+                    <div className='w-full mt-3 flex justify-end'>
                         <button className='bg-white group text-[20px] font-outfit text-blueClear py-2 px-4 rounded-full mt-4 uppercase hover:bg-blueClear hover:text-white flex items-center transition duration-300 ease-in-out'>
                             {button}
                             <ArrowRightIcon className='text-blueClear w-6 h-6 ml-2 group-hover:text-white group-hover:translate-x-1 transition duration-300 ease-in-out' />
