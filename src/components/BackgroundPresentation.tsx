7e27b6d4c3d896992c372c2df03fcaa28d3348a1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-08 14:25:02 +0100

    Update BackgroundPresentation.tsx

--- src/components/BackgroundPresentation.tsx
+++ src/components/BackgroundPresentation.tsx
@@ -41,7 +41,7 @@ const BackgroundPresentation = () => {
                         </li>
                     </ul>
                     <div className='w-full flex justify-end mt-3'>
-                        <button className='bg-white group text-[20px] font-outfit text-blueClear py-2 px-4 rounded-full mt-4 uppercase hover:bg-blueClear hover:text-white flex items-center transition duration-300 ease-in-out'>
+                        <button className='bg-white group  text-[14px] md:text-[20px] font-outfit text-blueClear py-2 px-4 rounded-full mt-4 uppercase hover:bg-blueClear hover:text-white flex items-center transition duration-300 ease-in-out'>
                             Connaitre l’adresse exacte
                             <ArrowRightIcon className='text-blueClear w-6 h-6 ml-2 group-hover:text-white group-hover:translate-x-1 transition duration-300 ease-in-out' />
                         </button>
