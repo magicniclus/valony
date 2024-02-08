9f71a02ee02901f5c0aced1f06a6795c6116c6c9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-08 13:42:40 +0100

    Create BluePresentation.tsx

---
+++ src/components/BluePresentation.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from 'react';
+import ForwardLine from './ForwardLine';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+
+type BluePresentationProps = {
+        navTitle: string;
+        navPourcentage: number;
+        title: string;
+        susTitle: string;
+        list: [string, string, string, string, string?];
+        button: string;
+    };
+
+const BluePresentation : React.FC<BluePresentationProps> = ({ navTitle, navPourcentage, title, susTitle, list, button }) => {
+    return (
+        <section className='flex'>
+            <div className='w-[121px] min-h-[625px] bg-blueClear px-4 py-6 hidden lg:flex flex-col items-center justify-between'>
+                <div className='w-full'>
+                    <div className='w-full'>
+                            <div className='flex justify-center min-w-max -translate-x-[72px]'>
+                                <p className='text-white text-[16px] rotate-[270deg] translate-y-32 uppercase font-outfit' style={{ width: "max-content" }}>&rsaquo; {navTitle}</p>
+                            </div>
+                        </div>
+                </div>
+                <ForwardLine pourcentage={navPourcentage} color="white" />
+            </div>
+            <div className='w-full flex md:flex-row flex-col'>
+                <div className='md:w-[50%] w-full min-h-[559px] overflow-hidden flex justify-between items-center bg-gray bg-cover bg-center' style={{"backgroundImage": "url(/images/oleron.jpg)"}}></div>
+                <div className='md:w-[50%] w-full min-h-[559px] overflow-hidden flex justify-between items-center bg-blueClearClear flex-col px-14 py-8'>
+                    <h2 className='text-[45px] font-bold text-blueClear leading-[50px]'>
+                        {title}
+                    </h2>
+                    <h3 className='font-outfit text-[17px] font-bold mt-3'>
+                        {susTitle}
+                    </h3>
+                    <ul className='text-[14px] font-outfit text-text mt-3'>
+                        {
+                            list.map((item, index) => (
+                                <li key={index} className='flex items-center'>
+                                    <span className='mr-2'>-</span>
+                                    {item}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    <div className='w-full mt-3'>
+                        <button className='bg-white group text-[20px] font-outfit text-blueClear py-2 px-4 rounded-full mt-4 uppercase hover:bg-blueClear hover:text-white flex items-center transition duration-300 ease-in-out'>
+                            {button}
+                            <ArrowRightIcon className='text-blueClear w-6 h-6 ml-2 group-hover:text-white group-hover:translate-x-1 transition duration-300 ease-in-out' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default BluePresentation;
