10311c71729389b577b28fc24952b1ff4fa501a3
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-08 13:42:42 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
       or: "#938664",
       background: "#A78E88",
       white: "#FFFFFF",
+      blueClear: "#256068",
+      blueClearClear: "#98AFB5",
       gray: "#312F39",
       beige: "#E6DFDE",
       beigeDark: "#A78E88"
