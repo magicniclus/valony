74eb43caee30c4ca7d3b2f02e1da36531a8781f6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-08 10:35:26 +0100

    Update ForwardLine.tsx

--- src/components/ForwardLine.tsx
+++ src/components/ForwardLine.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 type ForwardLineProps = {
     pourcentage: number;
+    color?: string;
 }
 
-const ForwardLine : React.FC<ForwardLineProps> = ({ pourcentage }) => {
+const ForwardLine : React.FC<ForwardLineProps> = ({ pourcentage, color }) => {
     return (
-        <div className='h-[64px] w-[2px] bg-or relative'>
-            <div style={{ top: `calc(${pourcentage}% - 4.5px)`, marginTop: '10px', marginBottom: '10px' }} className='h-[9px] w-[9px] rounded-full bg-or absolute left-1/2 transform -translate-x-1/2'></div>
+        <div className={`h-[64px] w-[2px] ${color ? "bg-"+color: "bg-or"} relative`}>
+            <div style={{ top: `calc(${pourcentage}% - 4.5px)`, marginTop: '10px', marginBottom: '10px' }} className={`h-[9px] w-[9px] rounded-full ${color ? "bg-"+color: "bg-or"} absolute left-1/2 transform -translate-x-1/2`}></div>
         </div>
     );
 };
