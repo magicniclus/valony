06a558b243d48183c169af2a5b6ec8f4ec0badbd
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-12 14:43:14 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,13 +33,18 @@ export default function RootLayout({
             }}
           />
           {/* Google Ads Script */}
-          <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WZP4H2GF');`,
-          }} />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16458670881"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-16458670881');
+              `,
+            }}
+          />
         </Head>
         <body className={inter.className}>
           {children}
