b6221eacd8426ad0fec5f8424835d5cef645ae40
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-12 11:14:19 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,24 +22,24 @@ export default function RootLayout({
     <StoreProvider>
       <html lang="fr">
         <Head>
-          {/* Google Tag Manager */}
-          <Script strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WZP4H2GF');`}
-          </Script>
-          {/* End Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WZP4H2GF');`,
+            }}
+          />
         </Head>
         <body className={inter.className}>
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZP4H2GF"
-            height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe>
-          </noscript>
           {/* End Google Tag Manager (noscript) */}
           {children}
+           <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZP4H2GF" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
+          />
         </body>
       </html>
     </StoreProvider>
