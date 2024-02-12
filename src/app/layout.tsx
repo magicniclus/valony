17b2620755ba20846e3397cf644dc6b0945e5593
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-12 11:02:14 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Analytics from "@/components/Analytics";
 
 import { StoreProvider } from "../redux/StoreProvider";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +20,26 @@ export default function RootLayout({
   return (
     <StoreProvider>
       <html lang="fr">
+        <head>
+          {/* Google Tag Manager */}
+          <Script strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WZP4H2GF');`}
+          </Script>
+          {/* End Google Tag Manager */}
+        </head>
         <body className={inter.className}>
-           <Analytics />
-           {children}</body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZP4H2GF"
+            height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
+          {children}
+        </body>
       </html>
     </StoreProvider>
   );
