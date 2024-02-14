7f7adfe8911dced1519515bd7511cda3b5fcd973
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-14 18:11:22 +0100

    Update Formulaire.tsx

--- src/components/Formulaire.tsx
+++ src/components/Formulaire.tsx
@@ -2,6 +2,7 @@
 
 import React, { use, useEffect } from 'react';
 import StepOne from './formStep/StepOne';
+import StepEng from './formStep/StepEng';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../redux/store';
@@ -11,20 +12,46 @@ import { stopFocus, focus as focusAction } from '@/redux/formFocusSlice';
 const Formulaire = () => {
 
     const focus = useSelector((state: RootState) => state.focus);
+    const language = useSelector((state: any) => state.language.language);    
 
     const dispatch = useDispatch();
 
-    return (
+    const fr = ()=>{
+        return (
         <form className='mt-10' 
-              onClick={() => dispatch(focusAction())} 
-              onBlur={(e) => {
-                  // Empêche le déclenchement de onBlur lors du passage entre les éléments du formulaire
-                  if (!e.currentTarget.contains(e.relatedTarget)) {
-                      dispatch(stopFocus());
-                  }
-              }}>
-            <StepOne />
+                    onClick={() => dispatch(focusAction())} 
+                    onBlur={(e) => {
+                        // Empêche le déclenchement de onBlur lors du passage entre les éléments du formulaire
+                        if (!e.currentTarget.contains(e.relatedTarget)) {
+                            dispatch(stopFocus());
+                        }
+                    }}>
+                    <StepOne />
         </form>
+        )
+    }
+
+    const eng = ()=>{
+        return (
+            <form className='mt-10' 
+                        onClick={() => dispatch(focusAction())} 
+                        onBlur={(e) => {
+                            // Empêche le déclenchement de onBlur lors du passage entre les éléments du formulaire
+                            if (!e.currentTarget.contains(e.relatedTarget)) {
+                                dispatch(stopFocus());
+                            }
+                        }}>
+                        <StepEng />
+            </form>
+        )
+    }
+
+    return (
+        <>
+            {
+                language === 'fr' ? fr() : eng()
+            }
+        </>
     );
 };
 
