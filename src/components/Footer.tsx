6076fd21e2a1b8e1eebea1728317d42a4630ef31
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-15 09:03:31 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,6 +41,7 @@ const Footer = () => {
         <a href={url} target="blank" key={name}
             onMouseEnter={() => handleMouseEnter(name)}
             onMouseLeave={handleMouseLeave}
+            onClick={() => handleMouseEnter(name)}
             className={`${activeIcon === name ? 'bg-white w-[17px] h-[17px] rounded-sm flex justify-center items-center' : 'bg-text w-[17px] h-[17px] flex justify-center items-center'}`}
             >
             <img src={`${basePath}${activeIcon === name ? `${iconName}-black.png` : `${iconName}.png`}`}
@@ -67,7 +68,7 @@ const Footer = () => {
                         height: 2px;
                     }
                 `}</style>
-                <footer className='py-10 px-16 w-full bg-text bg-black flex md:flex-row flex-col justify-between items-center'>
+                <footer className='py-10 px-2 md:px-16 w-full bg-text bg-black flex md:flex-row flex-col justify-between items-center'>
                     <img src="/images/logo-white.png" alt="logo" className="w-[158px] h-auto" />
                     <div className='h-full md:mt-0 mt-10'>
                         <div className='flex w-full justify-between'>
@@ -86,47 +87,33 @@ const Footer = () => {
     const en = ()=>{
         return (
              <>
-            <style jsx>{`
-                .button:after {
-                    content: '';
-                    position: absolute;
-                    right: 0;
-                    top: -10px;
-                    background-color: #938664;
-                    height: 1px;
-                    width: 100%;
-                }
-            `}</style>
-            <footer className='py-10 px-16 w-full bg-text bg-black flex justify-between items-center'>
-                <img src="/images/logo-white.png" alt="logo" className="w-[158px] h-auto" />
-                <div className='h-full'>
-                    <div className='flex w-full justify-between'>
-                       <a href="https://www.facebook.com/groupeduval" target='blank'>
-                                <img src="/icons/facebook.png" alt="facebook" className="w-[6px] h-auto" />
-                            </a>
-                            <a href="https://www.instagram.com/groupe_duval/" target='blank'>
-                                <img src="/icons/instagram.png" alt="instagram" className="w-[13px] h-auto" />
-                            </a>
-                            <a href=" https://twitter.com/groupe_duval" target='blank'>
-                                <img src="/icons/x.png" alt="x" className="w-[13px] h-auto" />   
-                            </a>
-                            <a href="https://www.linkedin.com/company/groupe-duval/" target='blank'>
-                                <img src="/icons/linkdin.png" alt="linkdin" className="w-[13px] h-auto" />
-                            </a>
-                            <a href=" https://fr.viadeo.com/fr/company/groupe-duval" target='blank'>
-                                <img src="/icons/pomme.png" alt="viadeo" className="w-[13px] h-auto" />
-                            </a>
-                            <a href="https://journaldericduval.fr/" target='blank'>
-                                <img src="/icons/wifi.png" alt="journal de ric duval" className="w-[13px] h-auto" />
-                            </a>
+                <style jsx>{`
+                    .button:after {
+                        content: '';
+                        position: absolute;
+                        right: 0;
+                        top: -10px;
+                        background-color: #938664;
+                        height: 1px;
+                        width: 100%;
+                    }
+                    .button:hover:after{
+                        height: 2px;
+                    }
+                `}</style>
+                <footer className='py-10 px-2 md:px-16 w-full bg-text bg-black flex md:flex-row flex-col justify-between items-center'>
+                    <img src="/images/logo-white.png" alt="logo" className="w-[158px] h-auto" />
+                    <div className='h-full md:mt-0 mt-10'>
+                        <div className='flex w-full justify-between'>
+                            {renderIconLinks()}
+                        </div>
+                        <div className='mt-9'>
+                            <a href="https://www.groupeduval.com/mentions-legales/" target='blank' className='button text-white font-outfit text-[14px] relative transition duration-100 ease-in-out hover:font-bold'>Mentions légales</a>
+                            <a href="/#hero" className='button text-white font-outfit text-[14px] relative ml-5 transition duration-100 ease-in-out hover:font-bold'>Contactez-nous</a>
+                        </div>
                     </div>
-                    <div className='mt-9'>
-                        <a  href="https://www.groupeduval.com/mentions-legales/" target='blank' className='button text-white font-outfit text-[14px] relative'>Legal Notice</a>
-                        <a href="/#hero" className='button text-white font-outfit text-[14px] relative ml-5'>Contact us</a>
-                    </div>
-                </div>
-            </footer>
-        </>
+                </footer>
+            </>
         )
     }
 
