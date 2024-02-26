974de55a19261cb2e77c9370b0f0c4fbae598e7d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-26 09:39:41 +0100

    Update EmailTemplate.tsx

--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
 import * as React from "react";
 
 // Définition de l'interface pour les props
@@ -6,31 +8,34 @@ interface EmailTemplateProps {
   email: string;
   telephone: string;
   indicatif?: string;
-date: string;
+  date: string;
 }
+
+const currentDate = format(new Date(), "dd/MM/yyyy", { locale: fr });
 
 export const EmailTemplate: React.FC<EmailTemplateProps> = ({
   nom,
   email,
   telephone,
-    indicatif,
-  date
+  indicatif,
+  date,
 }) => {
-    return (
-        <div>
-            <h2>Nouveau Prospect:</h2>
-            <h3>{date}</h3>
-            <ul>
-                <li>
-                    Nom: <span className="font-bold">{nom}</span>
-                </li>
-                <li>
-                    Email: <span className="font-bold">{email}</span>
-                </li>
-                <li>
-                    Téléphone: <span className="font-bold">{telephone}</span>
-                </li>
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h2>Nouveau Prospect depuis seaven-groupeduval.com:</h2>
+      <p>Date: {currentDate}</p>
+      <h3>{date}</h3>
+      <ul>
+        <li>
+          Nom: <span className="font-bold">{nom}</span>
+        </li>
+        <li>
+          Email: <span className="font-bold">{email}</span>
+        </li>
+        <li>
+          Téléphone: <span className="font-bold">{telephone}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
