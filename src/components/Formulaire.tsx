0a16456143a93a6692a0a291bec73a731c0c1bfe
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-25 10:23:55 +0200

    Update Formulaire.tsx

--- src/components/Formulaire.tsx
+++ src/components/Formulaire.tsx
@@ -1,58 +1,36 @@
 "use client";
 
-import React, { use, useEffect } from 'react';
-import StepOne from './formStep/StepOne';
-import StepEng from './formStep/StepEng';
+import StepEng from "./formStep/StepEng";
+import StepOne from "./formStep/StepOne";
 
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../redux/store';
-import { stopFocus, focus as focusAction } from '@/redux/formFocusSlice';
-
+import { focus as focusAction, stopFocus } from "@/redux/formFocusSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Formulaire = () => {
+  const focus = useSelector((state: RootState) => state.focus);
+  const language = useSelector((state: any) => state.language.language);
 
-    const focus = useSelector((state: RootState) => state.focus);
-    const language = useSelector((state: any) => state.language.language);    
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    const fr = ()=>{
-        return (
-        <form className='mt-10' 
-                    onClick={() => dispatch(focusAction())} 
-                    onBlur={(e) => {
-                        // Empêche le déclenchement de onBlur lors du passage entre les éléments du formulaire
-                        if (!e.currentTarget.contains(e.relatedTarget)) {
-                            dispatch(stopFocus());
-                        }
-                    }}>
-                    <StepOne />
-        </form>
-        )
-    }
-
-    const eng = ()=>{
-        return (
-            <form className='mt-10' 
-                        onClick={() => dispatch(focusAction())} 
-                        onBlur={(e) => {
-                            // Empêche le déclenchement de onBlur lors du passage entre les éléments du formulaire
-                            if (!e.currentTarget.contains(e.relatedTarget)) {
-                                dispatch(stopFocus());
-                            }
-                        }}>
-                        <StepEng />
-            </form>
-        )
-    }
-
+  const fr = () => {
     return (
-        <>
-            {
-                language === 'fr' ? fr() : eng()
-            }
-        </>
+      <form
+        className="mt-10"
+        onClick={() => dispatch(focusAction())}
+        onBlur={(e) => {
+          // Empêche le déclenchement de onBlur lors du passage entre les éléments du formulaire
+          if (!e.currentTarget.contains(e.relatedTarget)) {
+            dispatch(stopFocus());
+          }
+        }}
+      >
+        <StepOne />
+      </form>
     );
+  };
+
+  return <>{fr()}</>;
 };
 
 export default Formulaire;
