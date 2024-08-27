d81a05f0b560664d200b11302308865859ceabe8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-08-27 10:33:02 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,21 @@ export default function RootLayout({
       <html lang="fr">
         <head>
           <link rel="icon" href="logos/favicon.png" sizes="any" />
+
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-T7BQB9NK');
+              `,
+            }}
+          />
+          {/* End Google Tag Manager */}
+
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=AW-16683815620"
@@ -37,7 +52,20 @@ export default function RootLayout({
             }}
           />
         </head>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-T7BQB9NK"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
+
+          {children}
+        </body>
       </html>
     </StoreProvider>
   );
