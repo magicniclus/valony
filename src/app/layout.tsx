cbb2cb82239e7163b194e0a05cbd245d6fd06dba
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-09-06 09:43:16 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -22,49 +23,11 @@ export default function RootLayout({
       <html lang="fr">
         <head>
           <link rel="icon" href="logos/favicon.png" sizes="any" />
-
-          {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-T7BQB9NK');
-              `,
-            }}
-          />
-          {/* End Google Tag Manager */}
-
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=AW-16683815620"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'AW-16683815620');
-              `,
-            }}
-          />
         </head>
         <body className={inter.className}>
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-T7BQB9NK"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
-          {/* End Google Tag Manager (noscript) */}
-
           {children}
+
+          <GoogleTagManager gtmId="GTM-T7BQB9NK" />
         </body>
       </html>
     </StoreProvider>
